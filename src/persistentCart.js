de51c2bd9fc5a3e941c07515b239007d5667bc14
Dimitri <couwenberg@gmail.com>
2018-08-02 16:28:41 +1000

    chore:localstorage

--- src/persistentCart.js
+++ src/persistentCart.js
@@ -4,6 +4,7 @@ const persistentCart = () => {
   return {
     persist: (data) => localStorage.setItem(key, data),
     get: () => localStorage.getItem(key),
+    set: (data) => localStorage.setItem(key, data)
   }
 
 }
